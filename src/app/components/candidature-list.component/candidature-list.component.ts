807b1cffcbb70e11012805c359254f19e6b39971
Mabidou <msoum92@gmail.com>
2025-08-03 22:09:21 +0200

    Add Charlotte Soum to the candidatures list

--- src/app/components/candidature-list.component/candidature-list.component.ts
+++ src/app/components/candidature-list.component/candidature-list.component.ts
@@ -1,9 +1,10 @@
 export class CandidatureListComponent {
   candidatures = [
     { nom: 'Marie Dupont', poste: 'Développeuse', date: '2025-07-26' },
-    { nom: 'Jean Martin', poste: 'Designer UX', date: '2025-07-24' }
+    { nom: 'Jean Martin', poste: 'Designer UX', date: '2025-07-24' },
+    { nom: 'Charlotte Soum', poste: 'Growth hacker', date: '2025-07-24' }
   ];
 }
 
   
-
+  
