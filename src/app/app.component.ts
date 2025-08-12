f02676a7aec4dc34ca3e47d3fd12ca2a64c2b2f4
Mabidou <msoum92@gmail.com>
2025-08-12 15:27:52 +0200

    suite2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+//import { RouterOutlet } from '@angular/router';
+//import { ReactiveFormsModule } from '@angular/forms';
+//import { BrowserModule } from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, BrowserModule, ReactiveFormsModule]
+  template: '<h1>Ça marche 🎉</h1>',
+  //styleUrls: ['./app.component.css'],
+  //imports: [RouterOutlet, BrowserModule, ReactiveFormsModule]
 
 })
 export class AppComponent {
